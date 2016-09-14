e93a739a62a793a726f06bf8d5fe15401fd17f9c
codecademy123 <seanhe17@gmail.com>
2016-09-13 17:33:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 var score = 0;
+var scoreTag = document.getElementById("scoreTag");
 var addScore = function() {
     score += 10;
-    scoreTag = document.getElementById("scoreTag");
+    
     scoreTag.innerHTML = "Score: " + score;
+    localStorage.setItem('score', score);
 }
 addScore();
+var displayScore = function() {
+    var storedScore = localStorage.getItem('score');
+    scoreTag.innerHTML = "Score: " + storedScore;
+}
