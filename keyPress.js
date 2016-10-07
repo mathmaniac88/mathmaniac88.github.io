1b0d9cdeb560eb49c2b5a39f6e1325f282e96e20
codecademy123 <seanhe17@gmail.com>
2016-10-07 13:22:04 -0700

    Update keyPress.js

--- keyPress.js
+++ keyPress.js
@@ -1,5 +1,4 @@
 window.addEventListener("keydown", checkArrowKeys, true);
-var leftEl = 5;
 function checkArrowKeys(e) {
     if (e.keyCode == "37") {
         document.getElementById("h2").innerHTML = "The left arrow was pressed";
@@ -7,8 +6,6 @@ function checkArrowKeys(e) {
         document.getElementById("h2").innerHTML = "The up arrow was pressed";
     } else if (e.keyCode == "39") {
         document.getElementById("h2").innerHTML = "The right arrow was pressed";
-        document.getElementById("h2").style.margin-left = leftEl + "px";
-        leftEl += 5;
     } else if (e.keyCode == "40") {
         document.getElementById("h2").innerHTML = "The down arrow was pressed";
     }
