1171376af480cb32308d5b98cce86ebb8ce2a156
codecademy123 <seanhe17@gmail.com>
2016-10-22 09:48:19 -0700

    Update shorten_a_link.js

--- url/shorten_a_link.js
+++ url/shorten_a_link.js
@@ -1,5 +1,7 @@
 function shortenUrl () {
 	var input = document.getElementById("input");
 	var url = input.value;
+	var link = document.getElementById("link");
 	localStorage.setItem("url",url);
+	link.innerHTML = "Go to http://mypublicwebsites.tk/url/open_a_link.html to test out your link";
 }
