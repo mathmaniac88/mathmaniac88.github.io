5d981305733100dc18485244f35137bf45f5e219
codecademy123 <seanhe17@gmail.com>
2016-10-22 15:59:24 -0700

    Update shorten_a_link.js

--- url/shorten_a_link.js
+++ url/shorten_a_link.js
@@ -1,7 +1,6 @@
 function shortenUrl () {
-	var input = document.getElementById("input");
 	var url = input.value;
 	var link = document.getElementById("link");
 	localStorage.setItem("url",url);
-	link.innerHTML = "Go to http://mypublicwebsites.tk/url/open_a_link.html to test out your link";
+	document.getElementById("input").innerHTML = "Go to http://mypublicwebsites.tk/url/open_a_link.html to test out your link";
 }
