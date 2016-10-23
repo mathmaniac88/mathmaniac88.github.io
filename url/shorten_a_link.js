4e99c6d2a8abfc6233b0777289e978ccef6dff4b
codecademy123 <seanhe17@gmail.com>
2016-10-22 23:04:32 -0700

    Update shorten_a_link.js

--- url/shorten_a_link.js
+++ url/shorten_a_link.js
@@ -1,5 +1,5 @@
 function shortenUrl () {
-	var input = document.getElementById("inut");
+	var input = document.getElementById("input");
 	var url = input.value;
 	localStorage.setItem("url",url);
 	document.getElementById("link").innerHTML = "Go to http://mypublicwebsites.tk/url/url.html to test out your link";
