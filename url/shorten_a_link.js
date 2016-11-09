7815af8eb27bacf6f3aef5fd9e3784d9fe305cf5
codecademy123 <seanhe17@gmail.com>
2016-11-08 21:49:27 -0800

    Update shorten_a_link.js

--- url/shorten_a_link.js
+++ url/shorten_a_link.js
@@ -10,13 +10,15 @@ function log () {
 	var checkedValue = document.querySelector("#rememberTrue:checked").value;
 	var input = document.getElementById("input");
 	var url = input.value;
-	var i = 0;
+	var i = 1;
 	if (localStorage.getItem("i") === NaN) {
 		localStorage.setItem("i", i);
 	} else {
-		i = localStorage.getItem("i");
+		i = Number(localStorage.getItem("i"));
 	}
 	if(checkedValue === true) {
 		localStorage.setItem("url"+ i, url);
+		i++;
+		localStorage.setItem("i", i);
 	}
 }
