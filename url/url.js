546398c81ec87defa77569ceb40e26591136d683
codecademy123 <seanhe17@gmail.com>
2016-11-12 08:30:50 -0800

    Update url.js

--- url/url.js
+++ url/url.js
@@ -1,2 +1,5 @@
 var url = localStorage.getItem("url");
+if (url === "") {
+  document.getElementById("text").innerHTML = "Sorry, there was no link associated with this page currently"
+}
 window.open(url,"_self");
