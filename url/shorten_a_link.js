3df93f97aeda8851b419ab27f6bcf1a27d1e00f8
codecademy123 <seanhe17@gmail.com>
2016-11-13 12:54:29 -0800

    Update shorten_a_link.js

--- url/shorten_a_link.js
+++ url/shorten_a_link.js
@@ -6,34 +6,4 @@ function shortenUrl () {
 	document.getElementById("link").innerHTML= "http://mypublicwebsites.tk/url/url.html";
 	document.getElementById("text_after").innerHTML = " to test out your link";
 }
-function log () {
-	var checkedValue = document.querySelector("#rememberTrue:checked").value;
-	var input = document.getElementById("input");
-	var url = input.value;
-	var i = 1;
-	if (localStorage.getItem("i") === NaN) {
-		localStorage.setItem("i", i);
-	} else {
-		i = Number(localStorage.getItem("i"));
-	}
-	if(checkedValue === true) {
-		localStorage.setItem("url"+ i, url);
-		i++;
-		localStorage.setItem("i", i);
-	}
-}
-function printSavedTitles () {
-	var i = Number(localStorage.getItem("i"));
-	for(var j = 1;j<i+1;j++) {
-		var link = document.createElement("A");
-		var url = localStorage.getItem("url" + j);
-		var text = document.createTextNode(url);
-		link.appendChild(text);
-		document.body.appendChild(link);
-		document.getElementsByTagName("A")[0].setAttribute("href", "#");
-		document.getElementsByTagName("A")[0].setAttribute("onclick", "setCurrentUrl()");
-	}
-}
-function setCurrentUrl () {
-	
-}
+
