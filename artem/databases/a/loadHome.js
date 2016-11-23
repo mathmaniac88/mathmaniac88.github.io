5ac480035eb3ec5856fdaccddd03ffd78fec9b40
codecademy123 <seanhe17@gmail.com>
2016-11-23 11:52:30 -0800

    Update loadHome.js

--- artem/databases/a/loadHome.js
+++ artem/databases/a/loadHome.js
@@ -1,5 +1,6 @@
 var isSignedIn = localStorage.getItem("isSignedIn");
 var currentUser = localStorage.getItem("currentUser");
+console.log(currentUser);
 if(isSignedIn === "true") {
   if (currentUser === "Sean") {
     document.getElementById("greeting").innerHTML = "Hi " + currentUser;
