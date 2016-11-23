80da3011aa70d7e1efe6fb7de1630c8571b285ba
codecademy123 <seanhe17@gmail.com>
2016-11-22 21:48:21 -0800

    Update access.js

--- artem/databases/a/access.js
+++ artem/databases/a/access.js
@@ -1,3 +1,8 @@
 function authenticateData () {
-  var username = document.getElementById(
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  if(username === "awesomeguy" && password === "-12071242376-11212204105") {
+    localStorage.setItem("isSignedIn", "true");
+    localStorage.setItem("currentUser", "Sean");
+  }
 }
