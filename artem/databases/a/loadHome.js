8d4be64daee215b1440e1117ef4fc4c2e834e9bf
codecademy123 <seanhe17@gmail.com>
2016-11-22 22:39:25 -0800

    Update loadHome.js

--- artem/databases/a/loadHome.js
+++ artem/databases/a/loadHome.js
@@ -1,2 +1,9 @@
 var isSignedIn = localStorage.getItem("isSignedIn");
 var currentUser = localStorage.getItem("currentUser");
+if(isSignedIn === "true") {
+  if (currentUser === "Sean") {
+    document.getElementById("greeting").innerHTML = "Hi " + currentUser;
+  }
+} else {
+  window.location.replace("index.html");
+}
