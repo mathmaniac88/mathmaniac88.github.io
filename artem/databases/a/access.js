2c81d212df12677761989eb12df54a9d51527d2f
codecademy123 <seanhe17@gmail.com>
2016-11-25 20:02:49 -0800

    Update access.js

--- artem/databases/a/access.js
+++ artem/databases/a/access.js
@@ -5,6 +5,14 @@ function authenticateData () {
     localStorage.setItem("isSignedIn", "true");
     localStorage.setItem("currentUser", "Sean");
     window.location.replace("home.html");
+  } else if(username === "john4764" && password === "ethan101") {
+    localStorage.setItem("isSignedIn", "true");
+    localStorage.setItem("currentUser", "Ethan");
+    window.location.replace("home.html");
+  } else if(username === "hisname100" && password === "fartface3") {
+    localStorage.setItem("isSignedIn", "true");
+    localStorage.setItem("currentUser", "Artem");
+    window.location.replace("home.html");
   } else {
     document.getElementById("error").innerHTML = "The username or the password is incorrect";
     username = "";
