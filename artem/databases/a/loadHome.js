655e64bc33e68cc54701e27457f7752167f7c24e
codecademy123 <seanhe17@gmail.com>
2016-11-27 09:45:09 -0800

    Update loadHome.js

--- artem/databases/a/loadHome.js
+++ artem/databases/a/loadHome.js
@@ -2,7 +2,7 @@ var isSignedIn = localStorage.getItem("isSignedIn");
 var currentUser = localStorage.getItem("currentUser");
 var nameEl = document.getElementById("name");
 if(isSignedIn === "true") {
-  nameEl.innerHTML = currentUser;
+  load();
 } else {
   window.location.replace("index.html");
 }
@@ -10,4 +10,7 @@ function logOut () {
   localStorage.setItem("isSignedIn","false");
   localStorage.setItem("currentUser", "none");
   window.location.replace("index.html");
+}
+function load() {
+  nameEl.innerHTML = currentUser;
 }
