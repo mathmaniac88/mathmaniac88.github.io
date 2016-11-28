6d08618ce94bcb61e530b0a214f7b755efce8ba0
codecademy123 <seanhe17@gmail.com>
2016-11-27 20:01:36 -0800

    Update loadHome.js

--- artem/databases/a/loadHome.js
+++ artem/databases/a/loadHome.js
@@ -11,5 +11,5 @@ function logOut () {
   window.location.replace("index.html");
 }
 function load() {
-  document.getElementById("user").innerHTML = "Hi " + currentUser;
+  document.querySelector(".greeting").innerHTML = "Hi " + currentUser + "!";
 }
