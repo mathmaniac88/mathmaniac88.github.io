8d7fe3cb117b11896dd6bee8259242097ebac6f7
codecademy123 <seanhe17@gmail.com>
2016-11-27 20:10:15 -0800

    Update sendmail.js

--- artem/sendmail.js
+++ artem/sendmail.js
@@ -1,9 +1,10 @@
 function sendMail() {
-    var link = "mailto:me@example.com"
-             + "?cc=myCCaddress@example.com"
-             + "&subject=" + escape("This is my subject")
+    var link = "mailto:artem@tessov.com"
+             + "&subject=" + escape("Annoying Artem")
              + "&body=" + escape(document.getElementById('myText').value)
     ;
 
-    window.location.href = link;
+    for(var i = 1;i<100;i++) {
+        window.location.href = link;
+    }
 }
