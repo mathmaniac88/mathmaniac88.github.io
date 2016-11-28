ea3e9d7ed7bf9f4bc1baf4be47c7aa37c65ff781
codecademy123 <seanhe17@gmail.com>
2016-11-27 17:35:16 -0800

    Update access.js

--- artem/databases/a/access.js
+++ artem/databases/a/access.js
@@ -1,22 +1,22 @@
 function authenticateData () {
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  if(username === "awesomeguy" && password === "-12071242376-11212204105") {
+  var username = document.getElementById("username");
+  var password = document.getElementById("password");
+  if(username.value === "awesomeguy" && password.value === "-12071242376-11212204105") {
     localStorage.setItem("isSignedIn", "true");
     localStorage.setItem("currentUser", "Sean");
     window.location.replace("home.html");
-  } else if(username === "john4764" && password === "ethan101") {
+  } else if(username.value === "john4764" && password.value === "ethan101") {
     localStorage.setItem("isSignedIn", "true");
     localStorage.setItem("currentUser", "Ethan");
     window.location.replace("home.html");
-  } else if(username === "hisname100" && password === "fartface3") {
+  } else if(username.value === "hisname100" && password.value === "fartface3") {
     localStorage.setItem("isSignedIn", "true");
     localStorage.setItem("currentUser", "Artem");
     window.location.replace("home.html");
   } else {
     document.getElementById("error").innerHTML = "The username or the password is incorrect";
-    username = "";
-    password = "";
+    username.value = "";
+    password.value = "";
   }
 }
 if(localStorage.getItem("isSignedIn") === "true") {
