9954fd9ed5d80f8d41b34f450cc340cf278fe730
codecademy123 <seanhe17@gmail.com>
2017-01-12 17:44:27 -0800

    Update access.js

--- artem/databases/a/access.js
+++ artem/databases/a/access.js
@@ -1,46 +1,27 @@
-var users = [
-  {
-    name:"Sean",
-    username:"awesomeguy",
-    password:"awesomevhtccode"
-  },
-  {
-    name:"Ethan",
-    username:"john4764",
-    password:"ethan101"
-  },
-  {
-    name:"Artem",
-    username:"hisname100",
-    password:"fartface3"
-  },
-  {
-    name:"Taylor",
-    username:"taylord",
-    password:"taylor"
-  }
-  ]
 function authenticateData () {
   var username = document.getElementById("username");
   var password = document.getElementById("password");
-  var i = 0;
-  var verified = false;
-  while(i < users.length) {
-    if(username.value === users[i].username && password.value === users[i].password) {
-      localStorage.setItem("isSignedIn", true);
-      localStorage.setItem("currentUser", users[i].name);
-      var verified = true;
-      window.location.replace("home.html");
-    } else if(username.value === "" || password.value === "") {
-      password.value = "";
-      document.getElementById("error").innerHTML = "Please fill out all fields";
-    } else {
-      i++;
-    }
-  }
-  if(!verified) {
-    password.value = "";
-    document.getElement("error").innerHTML = "The username and/or password is incorrect";
+   if(username.value === "awesomeguy" && password.value === "awesomevhtccode") {
+     localStorage.setItem("isSignedIn", "true");
+     localStorage.setItem("currentUser", "Sean");
+     window.location.replace("home.html");
+   } else if(username.value === "john4764" && password.value === "ethan101") {
+     localStorage.setItem("isSignedIn", "true");
+     localStorage.setItem("currentUser", "Ethan");
+     window.location.replace("home.html");
+   } else if(username.value === "hisname100" && password.value === "fartface3") {
+     localStorage.setItem("isSignedIn", "true");
+     localStorage.setItem("currentUser", "Artem");
+     window.location.replace("home.html");
+   } else if(username.value === "taylord" && password.value === "taylor") {
+     localStorage.setItem("isSignedIn", "true");
+     localStorage.setItem("currentUser", "Taylor");
+     window.location.replace("home.html");
+   } else if(username.value === "" || password.value === "") {
+     document.getElementById("error").innerHTML = "Please fill out all fields";
+   } else {
+     document.getElementById("error").innerHTML = "The username and/or password is incorrect";
+   }
   }
 }
 if(localStorage.getItem("isSignedIn") === "true") {		
