7c98e1b75e09dd645f4633aadd36219063c7c954
codecademy123 <seanhe17@gmail.com>
2017-01-13 20:41:00 -0800

    Update calendar.js

--- artem/databases/a/calendar.js
+++ artem/databases/a/calendar.js
@@ -1,6 +1,15 @@
 var d = new Date();
 var m  = d.getMonth();
-var months = [["January", 31], ["Februrary", 28], ["March", 31], "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var months = [["January", 31], ["Februrary", 28], ["March", 31], ["April", 30], ["May", 31], ["June", 30], ["July", 31], ["August", 31], ["September", 30], ["October", 31], ["November", 30], ["December", 31]];
+if(d.getFullYear % 4 === 0) {
+  months[1][1] = 29;
+  if(d.getFullYear % 100 === 0) {
+    months[1][1] = 28;
+    if(d.getFullYear % 400 === 0) {
+      months[1][1] = 29;
+    }
+  }
+}
 numOfDays = months[m][1];
 m = months[m][0];
 document.querySelector("h1#month").innerHTML = m;
