02b43eba6bfd64d3f67aa12548e941b06516d210
codecademy123 <seanhe17@gmail.com>
2017-01-13 19:40:17 -0800

    Update calendar.js

--- artem/databases/a/calendar.js
+++ artem/databases/a/calendar.js
@@ -1,11 +1,26 @@
 var d = new Date();
 var m  = d.getMonth();
-var months = ["January", "Februrary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-m = months[m];
+var months = [["January", 31], ["Februrary", 28], ["March", 31], "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+numOfDays = months[m][1];
+m = months[m][0];
 document.querySelector("h1#month").innerHTML = m;
 var day = 1;
 var row = 1;
-var specificDate = new Date(d.getFullYear(), d.getMonth(), day);
-var tableCells = document.querySelectorAll("td#row" + row);
-tableCells[specificDate.getDay()].innerHTML = day;
-day++;
+var cellNum = 1;
+do {
+  do {
+    var specificDate = new Date(d.getFullYear(), d.getMonth(), day);
+    var tableCells = document.querySelectorAll("td#row" + row);
+    tableCells[specificDate.getDay()].innerHTML = day;
+    if(day === d.getDate()) {
+      tableCells[specificDate.getDay()].classList.add("currentDay");
+    }
+    if(day === numOfDays) {
+      cellNum = 7;
+      row = 6;
+    }
+    day++;
+    cellNum++;
+  } while(cellNum <= 7)
+  row++;
+} while(row <= 6)
