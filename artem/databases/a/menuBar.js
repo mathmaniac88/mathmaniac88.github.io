dcaf8db973a40118a4d97a23535e4da5dde18611
codecademy123 <seanhe17@gmail.com>
2017-01-13 21:07:34 -0800

    Update menuBar.js

--- artem/databases/a/menuBar.js
+++ artem/databases/a/menuBar.js
@@ -1,15 +1,8 @@
-var isSignedIn = localStorage.getItem("isSignedIn");
+var isSignedIn = Boolean(localStorage.getItem("isSignedIn"));
 var currentUser = localStorage.getItem("currentUser");
-if(isSignedIn === "true") {
-  load();
-} else {
-  window.location.replace("index.html");
-}
+document.getElementById("greeting").innerHTML = "Hi " + currentUser + "!";
 function logOut () {
   localStorage.setItem("isSignedIn","false");
   localStorage.setItem("currentUser", "none");
   window.location.replace("index.html");
-}
-function load() {
-  document.getElementById("greeting").innerHTML = "Hi " + currentUser + "!";
 }
